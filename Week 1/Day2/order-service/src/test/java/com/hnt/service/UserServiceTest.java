ba7ce160909f7642b8863d5acf860134d57f8310
unknown <cogjava3200@iiht.tech>
2022-09-12 17:39:41 +0530

    Week1 day2 commited

---
+++ Week 1/Day2/order-service/src/test/java/com/hnt/service/UserServiceTest.java
@@ -0,0 +1,53 @@
+package com.hnt.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.hnt.UserRepository;
+import com.hnt.entity.User;
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+	@InjectMocks
+	UserService service;
+	
+	@Mock
+	UserRepository repository;
+	@Test
+	void testSave() {
+		User user = new User();
+		user.setId(1);
+		user.setName("john");
+		when(repository.save(user)).thenReturn(user);//mock
+		service.save(user);
+		assertEquals(1,	user.getId());
+	}
+//	404
+	@Test
+	void testSaveForException() {
+		assertThrows(IllegalArgumentException.class, ()->{
+			User user = new User();
+			user.setName("Ram");
+			service.save(user);
+		});
+	}
+	@Test
+	void testdelete()
+	{
+	//	User user=new User();
+		service.delete(1);
+		verify(repository, times(1)).deleteById(1);
+		
+
+	}
+	
+
+}
